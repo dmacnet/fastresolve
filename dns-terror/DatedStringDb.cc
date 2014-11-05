31c2dd820c23a387aae3aab1bc286351bcf617e5
David MacKenzie <dmac@dbmacnet.com>
2014-11-04 22:42:46 -0500

    Support Berkeley DB 4.1+ and 6.x

--- dns-terror/DatedStringDb.cc
+++ dns-terror/DatedStringDb.cc
@@ -70,7 +70,7 @@ DatedStringDb::DatedStringDb(const char *dbhome, const char *storename)
   env->open(dbhome, DB_CREATE|DB_INIT_MPOOL|DB_PRIVATE, 0644);
   db = new Db(env, 0);
   db->open(
-#if DB_VERSION_MAJOR >= 5
+#if DB_VERSION_MAJOR >= 5 || (DB_VERSION_MAJOR == 4 && DB_VERSION_MINOR > 0)
 	   NULL,
 #endif
 	   storename, NULL, DB_BTREE, DB_CREATE, 0644);
